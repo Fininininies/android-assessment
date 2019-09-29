cbc29253aa526b7e5aca30fa37698ddf57122186
DFinies <dfinies@computest.nl>
2019-09-29 22:02:10 +0200

    Removed Duplicate Code, moved test rule to BaseTest

--- app/src/androidTest/java/com/abnamro/apps/referenceandroid/AssessmentTest.kt
+++ app/src/androidTest/java/com/abnamro/apps/referenceandroid/AssessmentTest.kt
@@ -3,6 +3,7 @@ package com.abnamro.apps.referenceandroid
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.rule.ActivityTestRule
+import com.abnamro.apps.referenceandroid.helpers.BaseTest
 import com.abnamro.apps.referenceandroid.helpers.MainPageTestDataProvider.HELLOTEXT
 import com.abnamro.apps.referenceandroid.helpers.MainPageTestDataProvider.SETTINGSTEXT
 import com.abnamro.apps.referenceandroid.helpers.MainPageTestDataProvider.TITLE
@@ -10,14 +11,9 @@ import com.abnamro.apps.referenceandroid.pageobject.MainPageObject
 import org.junit.Rule
 import org.junit.Test
 
-class AssessmentTest {
+class AssessmentTest: BaseTest() {
 
     private val mainPage = MainPageObject()
-
-    @Rule
-    @JvmField
-    val activityTestRule: ActivityTestRule<MainActivity> =
-        ActivityTestRule(MainActivity::class.java)
 
     @Test
     fun checkAppTitle() {
